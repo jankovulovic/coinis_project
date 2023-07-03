db979bd167367bb27402bd9968cd988876644bfe
Janko Vulovic <janko.vulovic@acme.co.me>
2023-07-03 18:26:50 +0200

    branislav profile save button

--- src/Componente/ProfilePage/ProfilePage.js
+++ src/Componente/ProfilePage/ProfilePage.js
@@ -1,14 +1,30 @@
-import React from "react";
-// import { useState, useMemo, useCallback, useContext } from "react";
-// import { useDropzone } from "react-dropzone";
+import React, { useState } from "react";
 import classes from "./ProfilePage.module.css";
 import Form from "../ProfilePage/Form/Form";
 import img from "../../Assets/profileimage.png";
 
 const ProfilePage = () => {
-  // const [fileUrl, setFileUrl] = useState(null);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    // Dodajte ostala polja podataka koja želite sačuvati
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  const handleSaveData = () => {
+    // Ovde možete izvršiti logiku za čuvanje podataka
+    console.log("Podaci su sačuvani:", formData);
+  };
+
   return (
-    <div className={classes.countainer}>
+    <div className={classes.container}>
       <div className={classes.profile}>
         <div className={classes.profile_info}>
           <h1>Profile</h1>
@@ -16,7 +32,7 @@ const ProfilePage = () => {
             You can set preferred display name, create your profile URL and
             manage other personal settings.
           </p>
-          <hr></hr>
+          <hr />
         </div>
         <div className={classes.profile_box}>
           <div className={classes.profile_box_img}>
@@ -29,7 +45,10 @@ const ProfilePage = () => {
             />
           </div>
           <div className={classes.profile_box_form}>
-            <Form />
+            <Form formData={formData} onInputChange={handleInputChange} />
+            <button className={classes.button} onClick={handleSaveData}>
+              Save
+            </button>
           </div>
         </div>
       </div>
