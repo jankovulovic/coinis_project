7e7250085d47af174d2ec783448ef6a84ab9d9aa
Janko Vulovic <janko.vulovic@acme.co.me>
2023-07-03 18:00:40 +0200

    profile_settings_page

--- src/Componente/ProfilePage/ProfilePage.js
+++ src/Componente/ProfilePage/ProfilePage.js
@@ -1,23 +1,25 @@
 import React from "react";
-import {useState, useMemo, useCallback, useContext} from "react";
-import {useDropzone}  from "react-dropzone";
+// import { useState, useMemo, useCallback, useContext } from "react";
+// import { useDropzone } from "react-dropzone";
 import classes from "./ProfilePage.module.css";
 import Form from "../ProfilePage/Form/Form";
 import img from "../../Assets/profileimage.png";
 
-
 const ProfilePage = () => {
-  const [fileUrl, setFileUrl] = useState(null);
+  // const [fileUrl, setFileUrl] = useState(null);
   return (
     <div className={classes.countainer}>
-   <div className={classes.profile}>
-    <div className={classes.profile_info}>
-      <h1>Profile</h1>
-      <p>You can set preferred display name, create your profile URL and manage other personal settings.</p>
-      <hr></hr>
-    </div>
-    <div className={classes.profile_box}>
-      <div className={classes.profile_box_img}>
+      <div className={classes.profile}>
+        <div className={classes.profile_info}>
+          <h1>Profile</h1>
+          <p>
+            You can set preferred display name, create your profile URL and
+            manage other personal settings.
+          </p>
+          <hr></hr>
+        </div>
+        <div className={classes.profile_box}>
+          <div className={classes.profile_box_img}>
             <img
               src={img}
               alt="profile upload"
@@ -25,15 +27,14 @@ const ProfilePage = () => {
               width={200}
               className={classes.profile_box_img_img}
             />
-      </div>
-      <div className={classes.profile_box_form}>
-        <Form />
+          </div>
+          <div className={classes.profile_box_form}>
+            <Form />
+          </div>
+        </div>
       </div>
     </div>
-   </div>
-   </div>
   );
 };
 
 export default ProfilePage;
-
