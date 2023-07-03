803e9554cae9943219d037291d4b0064765e5789
bane1901 <branislavkasalica@gmail.com>
2023-07-03 16:14:56 +0200

    profilepage

--- src/Componente/ProfilePage/ProfilePage.js
+++ src/Componente/ProfilePage/ProfilePage.js
@@ -1,9 +1,39 @@
+import React from "react";
+import {useState, useMemo, useCallback, useContext} from "react";
+import {useDropzone}  from "react-dropzone";
+import classes from "./ProfilePage.module.css";
+import Form from "../ProfilePage/Form/Form";
+import img from "../../Assets/profileimage.png";
+
+
 const ProfilePage = () => {
+  const [fileUrl, setFileUrl] = useState(null);
   return (
-    <>
-      <div>Profile Page</div>
-    </>
+    <div className={classes.countainer}>
+   <div className={classes.profile}>
+    <div className={classes.profile_info}>
+      <h1>Profile</h1>
+      <p>You can set preferred display name, create your profile URL and manage other personal settings.</p>
+      <hr></hr>
+    </div>
+    <div className={classes.profile_box}>
+      <div className={classes.profile_box_img}>
+            <img
+              src={img}
+              alt="profile upload"
+              height={200}
+              width={200}
+              className={classes.profile_box_img_img}
+            />
+      </div>
+      <div className={classes.profile_box_form}>
+        <Form />
+      </div>
+    </div>
+   </div>
+   </div>
   );
 };
 
 export default ProfilePage;
+
