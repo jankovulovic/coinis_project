bce60d6a859d1e35e8e23a7f8d5843ccd8489e29
Janko Vulovic <janko.vulovic@acme.co.me>
2023-07-15 20:13:40 +0200

    song post request header

--- src/Componente/AddingPage/SongForm.js
+++ src/Componente/AddingPage/SongForm.js
@@ -160,7 +160,7 @@ const SongForm = () => {
         data,
         {
           headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
+            "Content-Type": "application/json",
           },
         }
       );
